0eb149d69be2d4ff83434102f3a330e4219dcd9c
Rupesh Kumar <rupeshkumarsono552@gmail.com>
2025-06-01 12:35:43 +0530

    add Todo folder and make todo app

---
+++ Projects/2 ToDo List/todo.js
@@ -0,0 +1,39 @@
+let inputTodo = document.querySelector('#input-todo');
+
+let containerElement = document.querySelector('.todo-container');
+
+let dateElement = document.querySelector('#todo-date');
+
+let todoTask = localStorage.getItem('TodoList');
+let todoList = todoTask ? JSON.parse(todoTask) : [];
+
+displayItems();
+
+function addTodo() {
+  let todoItem = inputTodo.value;
+  let todoDate = dateElement.value;
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputTodo.value = '';
+  dateElement.value = '';
+  displayItems();
+
+  localStorage.setItem('TodoList', JSON.stringify(todoList));
+};
+
+function displayItems() {
+  let newHtml = '';
+
+  for(let i = 0; i < todoList.length; i++) {
+
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+      <span>${item}</span>
+      <span>${dueDate}</span>
+      <button onclick="todoList.splice(${i}, 1); displayItems();">Delete</button>
+      
+    `;
+  }
+  localStorage.removeItem
+
+  containerElement.innerHTML = newHtml;
+};
